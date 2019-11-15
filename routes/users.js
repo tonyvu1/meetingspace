34a52d0cef853014f98b50ded6a8d44f8290cab7
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2019-11-15 16:40:32 -0600

    ejs

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ const router = express.Router()
 
 //Login page
 router.get('/login', (req, res) => {
-    res.send('Login')
+    res.render('login')
 })
 
 //Register page
 router.get('/register', (req, res) => {
-    res.send('Register')
+    res.render('register')
 })
 
 module.exports = router
