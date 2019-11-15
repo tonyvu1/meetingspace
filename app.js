5f97d32bd6e835a424ae68a0133b99d223caab3e
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2019-11-15 17:16:54 -0600

    register page

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require("express");
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
+const mongoose = require("mongoose");
 
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 // EJS
-app.use(expressLayouts)
-app.set('view engine', 'ejs')
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 
-
+// Bodyparser
+app.use(express.urlencoded({extended: false}))
 
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
 const PORT = process.env.PORT || 5000;
 
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,53 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-//Login page
-router.get('/login', (req, res) => {
-    res.render('login')
-})
+// Login page
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
-//Register page
-router.get('/register', (req, res) => {
-    res.render('register')
-})
+// Register page
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
-module.exports = router
+// Register Handle / POST
+router.post("/register", (req, res) => {
+    // pulling out info in const { }
+  const { name, email, password, password2 } = req.body;
+
+  let errors = []
+
+  // Check required fields
+  if(!name || !email || !password || !password2) {
+      errors.push({msg: 'Please fill in all fields'})
+  }
+
+  // Check passwords match
+  if(password !== password2) {
+      errors.push({msg: 'Passwords do not match'})
+  }
+
+  // Check password length
+  if(password.length < 6) {
+      errors.push({msg: 'Password should be at least 6 characters long'})
+  }
+
+  // If any of these errors is true, re-render this register page.
+  // Also, we are going to pass in values to the re-rendered page.
+  if(errors.length > 0) {
+    res.render('register', {
+        // Pass in values and do not erase values from previous sent form
+        errors,
+        name,
+        email,
+        password,
+        password2
+    })
+  }
+  else {
+      res.send('pass')
+  }
+});
+
+module.exports = router;
