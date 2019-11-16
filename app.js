db57262b7386302552e19e8119459f297fa4d946
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2019-11-16 14:37:34 -0600

    test deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 var favicon = require('serve-favicon');
 
 const app = express();
-app.use(favicon(__dirname + '/views/favicon.ico'));
+app.use(favicon(__dirname + '/views/icons/favicon.ico'));
 
 // Passport Config
 require('./config/passport')(passport);
